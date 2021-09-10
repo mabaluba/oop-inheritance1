fb91712f9c666d7f7c20927c158b450e7788af90
Макс <yashanty@gmail.com>
2021-09-10 16:04:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace oop_inheritance1
+{
+    class Printer
+    {
+        public void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class PrintRed:Printer
+    {
+        public PrintRed()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        
+        /*public void RedText(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            //Print(value);
+        }*/
+    }
+    class PrintBlue:Printer
+    {
+        public PrintBlue()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            //Console.Clear();
+            //Console.BackgroundColor = ConsoleColor.White;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Whaaat?!!!?!!";
+            Printer x = new();
+            x.Print(text);
+            PrintBlue b = new();
+            b.Print(text);
+            PrintRed a = new();
+            a.Print(text);
+            x = a;
+            x.Print(text);
+            
+            x = b;// печатает красный, а должен синий... не понятно почему
+            x.Print(text);
+        }
+    }
+}
